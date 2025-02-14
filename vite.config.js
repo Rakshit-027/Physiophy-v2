81f4043761084d41ba26d60a0ef40bff328f8311
Raksit-027 <rakshitwaghmare@gmail.com>
2025-02-15 03:01:12 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'; // Correct plugin for React
 import tailwindcss from 'tailwindcss'; // TailwindCSS should be used in a PostCSS config, not here
 
-export default {
-  build: {
-    rollupOptions: {
-      external: ['styled-components']
-    }
-  }
-}
+export default defineConfig({
+  plugins: [
+    react(), // Correct way to add React plugin
+  ],
+});
